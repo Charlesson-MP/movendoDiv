8f17cf3ad33b18affa6894e686fad28b71aecfb3
Charlesson Mendes Pereira <86690606+Charlesson-MP@users.noreply.github.com>
2024-10-10 19:59:45 -0300

    Corrigindo bugs na aplicação

--- script.js
+++ script.js
@@ -6,36 +6,55 @@ const btn_down = document.querySelector('#btn_down')
 const carro = document.querySelector('#carro')
 const botoes = document.querySelector('#botoes')
 
+let anima = null
+let larguraTela = 0
+let alturaTela = 0
+let tamCarro = 0
+
 const init = () => {
     carro.style.position = 'relative'
-    carro.style.left = '500px'
-    carro.style.top = '100px'
+    carro.style.left = '0px'
+    carro.style.top = '0px'
+    carro.style.width = '50px'
+    carro.style.height = ' 50px'
+    botoes.style.height = '200px'
+    tamCarro = parseInt(carro.style.width)
+    larguraTela = window.innerWidth - tamCarro
+    alturaTela = window.innerHeight - tamCarro - parseInt(botoes.style.height)
 }
 
-let anima = null
-
 const moverHorizontal = (dir) => {
-    if(parseInt(carro.style.left) == 10 || (parseInt(carro.style.left) == window.innerWidth - carro.offsetWidth - 7 - 10)){
-        clearInterval(anima)
+    if(parseInt(carro.style.left) >= larguraTela) {
+        dir = -1
+    }else if(parseInt(carro.style.left) <= 0) {
+        dir = 1
     }
+
     carro.style.left = `${parseInt(carro.style.left) + (10*dir)}px`
+
+    anima = setTimeout(moverHorizontal, 20, dir)
 }
 
 const moverVertical = (dir) => {
-    if(parseInt(carro.style.top) == 10 || parseInt(carro.style.top) == 350){
-        clearInterval(anima)
+    if(parseInt(carro.style.top) >= alturaTela) {
+        dir = -1
+    }else if(parseInt(carro.style.top) <= 0) {
+        dir = 1
     }
+
     carro.style.top = `${parseInt(carro.style.top) + (10*dir)}px`
+
+    anima = setTimeout(moverVertical, 20, dir)
 }
 
 btn_up.addEventListener('click', () => {
     clearInterval(anima)
-    anima = setInterval(moverVertical, 100, -1)
+    moverVertical(-1)
 })
 
 btn_left.addEventListener('click', () => {
         clearInterval(anima)
-        anima = setInterval(moverHorizontal, 100, -1)
+        moverHorizontal(-1)
 })
 
 btn_stop.addEventListener('click', () => {
@@ -44,12 +63,12 @@ btn_stop.addEventListener('click', () => {
 
 btn_right.addEventListener('click', () => {
     clearInterval(anima)
-    anima = setInterval(moverHorizontal, 100, 1)
+    moverHorizontal(1)
 })
 
 btn_down.addEventListener('click', () => {
     clearInterval(anima)
-    anima = setInterval(moverVertical, 100, 1)
+    moverVertical(1)
 })
 
 window.onload = init()
